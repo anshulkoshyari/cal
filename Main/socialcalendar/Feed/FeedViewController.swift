11a02705412088dda82ec12ba07c5e9f3f3cd1f5
Anshul Koshyari <koshyaria@gmail.com>
2025-01-21 20:01:25 +0530

    Added skeleton for Feed view controller

---
+++ Main/socialcalendar/Feed/FeedViewController.swift
@@ -0,0 +1,14 @@
+//
+//  FeedViewController.swift
+//  socialcalendar
+//
+//  Created by AK on 21/01/25.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
